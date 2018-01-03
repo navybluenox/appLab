8f1d76a3c2b8b61b119a2982d47e68c1b2fcb169
navybluenox <navyblueliktt08@gmail.com>
2018-01-03 22:41:05 +0900

    2018/01/03 22:41:01

--- src/ts/client/onOpen.tsx
+++ src/ts/client/onOpen.tsx
@@ -1,6 +1,9 @@
 /* reactとreact-domの読み込み */
-/*
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as jQuey from 'jquery';
-*/
+import * as $ from 'jquery';
+
+function test():void{
+    var str:string = "" + $("#code").val();
+    eval(str);
+}
